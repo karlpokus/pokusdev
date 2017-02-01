dfceab28b1b984e357e9705242aa47c8f80ab344
Karl Pokus <karl.pokus@gmail.com>
2017-02-01 10:13:53 +0000

    added some routes

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 var http = require('http'),
     server = http.createServer(),
+    url = require('url'),
     port = process.env.PORT || 3000;
-
-function reply(req, res) {
-  res.end('this is not a 404');
-}
 
 function running() {
   console.log('server running on ', port);
 }
 
-server.on('request', reply);
-server.listen(port, running);
+server
+  .on('request', function(req, res){
+    var pathname = url.parse(req.url).pathname;
+    
+    // router
+    if (pathname === '/') {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('this is not a 404');
+      
+    } else if (pathname === '/users') {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('here may be users one day');
+      
+    } else {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('this IS a 404');
+    }
+    
+  }).listen(port, running);
